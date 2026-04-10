df463d5c245442b67965ce4a44a998cb6b6f655f
Kartavya Panwar <kartavyasinghpanwar44@gmail.com>
2026-04-10 21:02:58 +0530

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(async () => {
-  const plugins = [react(), tailwindcss()];
-  try {
-    // @ts-expect-error: Dynamic import for optional plugin
-    const m = await import('./.vite-source-tags.js');
-    plugins.push(m.sourceTags());
-  } catch {
-    // optional plugin
-  }
-  return { plugins };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: './',
 })
