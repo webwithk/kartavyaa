d444a92400414a36c3ea2afa6ed155d7e5de5a32
Kartavya Panwar <kartavyasinghpanwar44@gmail.com>
2026-04-11 17:25:06 +0530

    check env

--- api/_supabase.js
+++ api/_supabase.js
@@ -3,5 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log("SUPABASE URL:", supabaseUrl);
+console.log("SUPABASE KEY:", supabaseKey);
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase
